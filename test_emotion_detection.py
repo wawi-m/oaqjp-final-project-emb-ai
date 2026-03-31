3ce9b87bf80b08a9148d1e26582f37f1eba08d74
Wawi <122799427+wawi-m@users.noreply.github.com>
2026-03-31 14:18:57 +0300

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,36 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+    def test_anger(self):
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result["dominant_emotion"], "anger")
+
+    def test_disgust(self):
+        result = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result["dominant_emotion"], "disgust")
+
+    def test_fear(self):
+        result = emotion_detector("I am afraid this will happen")
+        self.assertEqual(result["dominant_emotion"], "fear")
+
+    def test_sadness(self):
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result["dominant_emotion"], "sadness")
+
+
+def test_emotion_detector():
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestEmotionDetector)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    test_sentiment_analyzer()
+
+unittest.main()
